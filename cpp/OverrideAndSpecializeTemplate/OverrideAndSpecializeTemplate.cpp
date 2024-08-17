a3581817fcb173552597351f436cec8845cac938
MindiashviliRoman <mindiashvilirr@gmail.com>
2024-08-17 22:47:12 +0300

    refactoring

--- cpp/OverrideAndSpecializeTemplate/OverrideAndSpecializeTemplate.cpp
+++ cpp/OverrideAndSpecializeTemplate/OverrideAndSpecializeTemplate.cpp
@@ -3,28 +3,28 @@
 
 using namespace std;
 template<class T>
-void SomeMethod(T a, T b) //Some Method 1
+void SomeFunc(T a, T b) //Some function 1
 {
-    cout << "some method" << endl;
+    cout << "some function" << endl;
 }
 
 template<class T, class D>
-void SomeMethod(T a, D b) //Some Method 2
+void SomeFunc(T a, D b) //Some function 2
 {
-    cout << "overrided some method" << endl;
+    cout << "overrided some function" << endl;
 }
 
-template<> //full specialize of template (Some Method 2)
-void SomeMethod<int, int>(int a, int b)
+template<> //full specialize of template (Some function 2)
+void SomeFunc<int, int>(int a, int b)
 {
-    cout << "Some method with both int parameters" << endl;
+    cout << "Some function with both int parameters" << endl;
 }
 
 int main()
 {
-    //Called Some method 1. Couse in the first was selected overriding of methods and after - specializing.
-    //After overriding method - was selected some method 1 and it have not specializings.
-    SomeMethod(1, 2);
+    //Called Some function 1. Couse in the first was selected overriding of functions and after - specializing.
+    //After overriding function - was selected some function 1 and it have not specializings.
+    SomeFunc(1, 2);
 
     return 0;
 }
